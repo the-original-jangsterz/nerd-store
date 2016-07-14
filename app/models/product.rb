f40ce93ae42a999a86ab158541d3007144821f35
Peter Jang <peter.x.jang@gmail.com>
2016-07-13 21:30:34 -0500

    Add model methods for discount items, tax, and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_f < 100.0
+      message = "Discount Item!"
+    else
+      message = "On Sale!"
+    end
+    message
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
+
+  def price_class_name
+    if sale_message == "Discount Item!"
+      class_name = "discount"
+    else
+      class_name = ""
+    end
+  end
 end
