1194b274a5fdddf3a5b7497b3f88c05dd0d50c05
Peter Jang <peter.x.jang@gmail.com>
2016-07-21 18:44:35 -0500

    Save all order attributes and show them on order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
     order.save
     flash[:success] = "Order created!"
