926705b40a653fbcfe8dfc89762260b6eeb87cb1
Peter Jang <peter.x.jang@gmail.com>
2016-07-24 16:45:08 -0500

    Add product/category associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :categorized_products
+  has_many :categories, through: :categorized_products
 
   def discounted?
     price.to_f < 100.0
