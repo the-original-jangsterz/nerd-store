04f5c21c8b0d197222a71b1b294451392764ef79
Peter Jang <peter.x.jang@gmail.com>
2016-07-26 19:28:55 -0500

    Refactor order calculations to model method

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def calculate_totals(input_carted_products)
+    subtotal = 0
+    input_carted_products.each do |carted_product|
+      subtotal += carted_product.quantity * carted_product.product.price
+    end
+    tax = subtotal * 0.09
+    total = subtotal + tax
+    update(subtotal: subtotal, tax: tax, total: total)
+  end
 end
