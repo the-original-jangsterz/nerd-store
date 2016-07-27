ac265ddcec70f7e0a0f8d9a330b59e4c694aac8a
Peter Jang <peter.x.jang@gmail.com>
2016-07-26 19:36:21 -0500

    Show cart count on nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :calculate_cart_count
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
@@ -10,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     redirect_to '/login' unless current_user
+  end
+
+  def calculate_cart_count
+    if current_user
+      @cart_count = current_user.carted_products.where(status: 'carted').count
+    else
+      @cart_count = 0
+    end
   end
 end
